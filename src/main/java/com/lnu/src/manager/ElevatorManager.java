5d0286756ad2fc385d623588da316249ab827bbe
isas <isas@roku.com>
2022-04-18 11:51:57 +0300

    update strategies initialization

--- src/main/java/com/lnu/src/manager/ElevatorManager.java
+++ src/main/java/com/lnu/src/manager/ElevatorManager.java
@@ -2,15 +2,17 @@ package com.lnu.src.manager;
 
 import com.lnu.src.model.Floor;
 import com.lnu.src.strategy.ElevatorStrategy;
+import com.lnu.src.strategy.StopsOnDemandStrategy;
 import com.lnu.src.strategy.StopsOnEveryFloorStrategy;
 import com.lnu.src.thread.Elevator;
 
 import java.util.List;
+import java.util.Objects;
 
 public class ElevatorManager {
 
-  private final ElevatorStrategy stopsOnDemandStrategy = new StopsOnEveryFloorStrategy();
-  private final ElevatorStrategy stopsOnEveryFloorStrategy = new StopsOnEveryFloorStrategy();
+  private ElevatorStrategy stopsOnDemandStrategy;
+  private ElevatorStrategy stopsOnEveryFloorStrategy;
   private final List<Floor> floors;
 
   public ElevatorManager(List<Floor> floors) {
@@ -18,11 +20,25 @@ public class ElevatorManager {
   }
 
   public Elevator createStopsOnDemandElevator() {
-    return new Elevator(floors, stopsOnDemandStrategy);
+    return new Elevator(floors, getStopsOnDemandStrategy());
   }
 
   public Elevator createStopsOnEveryFloorElevator() {
-    return new Elevator(floors, stopsOnEveryFloorStrategy);
+    return new Elevator(floors, getStopsOnEveryFloorStrategy());
+  }
+
+  private ElevatorStrategy getStopsOnDemandStrategy() {
+    if (Objects.isNull(stopsOnDemandStrategy)) {
+      stopsOnDemandStrategy = new StopsOnDemandStrategy();
+    }
+    return stopsOnDemandStrategy;
+  }
+
+  private ElevatorStrategy getStopsOnEveryFloorStrategy() {
+    if (Objects.isNull(stopsOnEveryFloorStrategy)) {
+      stopsOnEveryFloorStrategy = new StopsOnEveryFloorStrategy();
+    }
+    return stopsOnEveryFloorStrategy;
   }
 
   public void changeSpeed(Elevator elevator, int newSpeed) {
